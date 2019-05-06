c14784d6d287fabc9ef17debbf4f51a27c9e7bc8
Matt Schnee <matthew.schnee@gmail.com>
2019-05-06 13:02:11 -0700

    Update azure configuration

--- api/src/Service.ts
+++ api/src/Service.ts
@@ -1,22 +1,40 @@
-import { createServer, Server } from 'http'
+import { createServer, Server } from "http";
+import { join } from "path";
+import { pathToFileURL } from "url";
 
-import express, { Application } from 'express'
-import { initialize } from 'express-openapi'
+import express, { Application } from "express";
+import { initialize } from "express-openapi";
 
 export default class Service {
-    private app: Application
-    private server: Server
+    private app: Application;
+    private server: Server;
 
-    public async boot() {
-        this.app = express()
-        this.server = createServer(this.app)
+    public async boot () {
+        this.app = express();
+        this.server = createServer(this.app);
     }
 
-    public async start() {
-        return new Promise(resolve => this.server.listen(process.env.PORT || 3000, resolve))
+    public async start () {
+        initialize({
+            app: this.app,
+            apiDoc: {
+                openapi: "3.0.0",
+                info: {
+                    title: "Know-Thyself Api",
+                    version: "0.0.1"
+                },
+                paths: {}
+            },
+            paths: join(__dirname, "openapi"),
+            pathsIgnore: /(\.controller.[tj]s$|__tests?__|lib\/)/
+        });
+
+        return new Promise(resolve =>
+            this.server.listen(process.env.PORT || 3000, resolve)
+        );
     }
 
-    public async stop() {
-        this.server.close()
+    public async stop () {
+        this.server.close();
     }
 }
--- certs/create-ssl.sh
+++ certs/create-ssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat <<'EOF' >> local.know-yourself.com.csr.cnf
+test -f local.know-yourself.com.csr.cnf || cat <<'EOF' >> local.know-yourself.com.csr.cnf
 [req]
 default_bits = 2048
 prompt = no
@@ -16,7 +16,7 @@ emailAddress=tech@know-yourself.com
 CN = local.know-yourself.com
 EOF
 
-cat <<'EOF' >> local.know-yourself.com.v3.ext
+test -f local.know-yourself.com.v3.ext || cat <<'EOF' >> local.know-yourself.com.v3.ext
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -28,8 +28,8 @@ DNS.2 = local.know-yourself.com
 DNS.3 = *.local.know-yourself.com
 EOF
 
-openssl genrsa -des3 -out self-signed-rootCA.key 2048
-openssl req -x509 -new -nodes -key self-signed-rootCA.key -sha256 -days 1024 -out self-signed-rootCA.pem -config <( cat local.know-yourself.com.csr.cnf )
+test -f self-signed-rootCA.key || openssl genrsa -des3 -out self-signed-rootCA.key 2048
+test -f self-signed-rootCA.pem || openssl req -x509 -new -nodes -key self-signed-rootCA.key -sha256 -days 1024 -out self-signed-rootCA.pem -config <( cat local.know-yourself.com.csr.cnf )
 
 openssl req -new -sha256 -nodes -out local.know-yourself.com.csr -newkey rsa:2048 -keyout local.know-yourself.com.key -config <( cat local.know-yourself.com.csr.cnf )
 openssl x509 -req -in local.know-yourself.com.csr -CA self-signed-rootCA.pem -CAkey self-signed-rootCA.key -CAcreateserial -out local.know-yourself.com.crt -days 500 -sha256 -extfile local.know-yourself.com.v3.ext
